717e329a17bbdff5af8077f36bd7c30043264861
Ashish Bagate <83308580+ashish000777@users.noreply.github.com>
2024-04-28 14:14:47 +0530

    Update parallel_bfs_dfs.cpp

--- parallel_bfs_dfs.cpp
+++ parallel_bfs_dfs.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <omp.h>
+
+// #include <bits/stdc++.h>
 #include <ctime>
 #include <iomanip>
+
 #include <vector>
 #include <queue>
 #include <unordered_set>
@@ -10,69 +13,71 @@ using namespace std;
 
 void BFS(vector<vector<int>>& graph, int start)
 {
-    int n = graph.size();
-    vector<bool> visited(n, false);
+    int n=graph.size();
+    vector<bool> visited(n,false);
     queue<int> q;
 
-    visited[start] = true;
+    visited[start]=true;
     q.push(start);
 
-    while (!q.empty())
+    while(!q.empty())
     {
-        int current = q.front();
+        int current=q.front();
         q.pop();
-        cout << current << " ";
+        cout<<current<<" ";
 
-        for (int i = 0; i < graph[current].size(); i++)
+        for(int i=0; i<graph[current].size(); i++)
         {
-            if (visited[graph[current][i]] == false)
+            if(visited[graph[current][i]]==false)
             {
-                visited[graph[current][i]] = true;
+                visited[graph[current][i]]=true;
                 q.push(graph[current][i]);
             }
         }
+
     }
 }
 
 void parallelBFS(vector<vector<int>>& graph, int start)
 {
-    int n = graph.size();
-    vector<bool> visited(n, false);
-    queue<int> q;
+        int n=graph.size();
+        vector<bool> visited(n,false);
+        queue<int> q;
 
-    visited[start] = true;
-    q.push(start);
+        visited[start]=true;
+        q.push(start);
 
-    while (!q.empty())
+    while(!q.empty())
     {
-#pragma omp parallel
+        #pragma omp parallel
         {
-            int current = q.front();
+            int current=q.front();
             q.pop();
-            cout << current << " ";
-#pragma omp for
-            for (int i = 0; i < graph[current].size(); i++)
+            cout<<current<<" ";
+            #pragma omp for
+            for(int i=0; i<graph[current].size(); i++)
             {
-#pragma omp critical
-                if (visited[graph[current][i]] == false)
+                #pragma omp critical
+                if(visited[graph[current][i]]==false)
                 {
-                    visited[graph[current][i]] = true;
+                    visited[graph[current][i]]=true;
                     q.push(graph[current][i]);
                 }
             }
+
         }
     }
 }
 
 void DFSutil(vector<vector<int>>& graph, int current, vector<bool>& visited)
 {
-    visited[current] = true;
-    cout << current << " ";
+    visited[current]=true;
+    cout<<current<<" ";
 
-    for (int i = 0; i < graph[current].size(); i++)
+    for(int i=0; i<graph[current].size(); i++)
     {
-        int adjacent = graph[current][i];
-        if (!visited[adjacent])
+        int adjacent=graph[current][i];
+        if(!visited[adjacent])
         {
             DFSutil(graph, adjacent, visited);
         }
@@ -81,22 +86,23 @@ void DFSutil(vector<vector<int>>& graph, int current, vector<bool>& visited)
 
 void DFS(vector<vector<int>>& graph, int start)
 {
-    int n = graph.size();
-    vector<bool> visited(n, false);
+    int n= graph.size();
+    vector<bool> visited(n,false);
 
     DFSutil(graph, start, visited);
 }
 
+
 void parallelDFSutil(vector<vector<int>>& graph, int current, vector<bool>& visited)
 {
-    visited[current] = true;
-    cout << current << " ";
+    visited[current]=true;
+    cout<<current<<" ";
 
-#pragma omp parallel for
-    for (int i = 0; i < graph[current].size(); i++)
+    #pragma omp parallel for
+    for(int i=0; i<graph[current].size(); i++)
     {
-        int adjacent = graph[current][i];
-        if (!visited[adjacent])
+        int adjacent=graph[current][i];
+        if(!visited[adjacent])
         {
             DFSutil(graph, adjacent, visited);
         }
@@ -105,74 +111,83 @@ void parallelDFSutil(vector<vector<int>>& graph, int current, vector<bool>& visi
 
 void parallelDFS(vector<vector<int>>& graph, int start)
 {
-    int n = graph.size();
-    vector<bool> visited(n, false);
+    int n= graph.size();
+    vector<bool> visited(n,false);
 
     parallelDFSutil(graph, start, visited);
 }
 
 int main()
 {
-    int numNodes, numEdges;
-    cout << "Enter the number of nodes: ";
-    cin >> numNodes;
+    int n; // Number of vertices
+    cout << "Enter the number of vertices: ";
+    cin >> n;
 
-    vector<vector<int>> graph(numNodes);
+    vector<vector<int>> graph(n); // Create a graph with n vertices
 
-    cout << "Enter the number of edges: ";
-    cin >> numEdges;
-
-    cout << "Enter the edges (node1 node2):" << endl;
-    for (int i = 0; i < numEdges; ++i)
-    {
-        int node1, node2;
-        cin >> node1 >> node2;
-        graph[node1].push_back(node2);
-        graph[node2].push_back(node1); // if undirected graph
+    // Read adjacency list from user input
+    for (int i = 0; i < n; i++) {
+        cout << "Enter neighbors of vertex " << i << " (end with a negative number): ";
+        while (true) {
+            int neighbor;
+            cin >> neighbor;
+            if (neighbor < 0) {
+                break;
+            }
+            graph[i].push_back(neighbor);
+        }
     }
 
-    int startNode;
-    cout << "Enter the start node for BFS and DFS: ";
-    cin >> startNode;
+    // Print the graph to verify the input
+    cout << "The graph adjacency list is:\n";
+    for (int i = 0; i < graph.size(); i++) {
+        cout << i << ": ";
+        for (int neighbor : graph[i]) {
+            cout << neighbor << " ";
+        }
+        cout << endl;
+    }
 
     clock_t start, end;
     double total1, total2;
 
-    start = clock();
-    cout << "Sequential BFS output : ";
-    BFS(graph, startNode);
-    end = clock();
+    start=clock();
+    cout<<"sequential BFS output : ";
+    BFS(graph,0);
+    end=clock();
 
-    total1 = double(end - start) / double(CLOCKS_PER_SEC);
-    cout << "\nSequential time : " << fixed << setprecision(10) << total1;
+    total1=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nSequential time : "<<fixed<<setprecision(10)<<total1;
 
-    start = clock();
-    cout << "\nParallel BFS output : ";
-    parallelBFS(graph, startNode);
-    end = clock();
+    start=clock();
+    cout<<"\nparallel BFS output : ";
+    parallelBFS(graph,0);
+    end=clock();
 
-    total2 = double(end - start) / double(CLOCKS_PER_SEC);
-    cout << "\nParallel time : " << fixed << setprecision(10) << total2;
+    total2=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nParallel time : "<<fixed<<setprecision(10)<<total2;
 
-    cout << "\nSpeedup : " << total1 / total2;
+    cout<<"\nSpeedup : "<<total1/total2;
 
-    start = clock();
-    cout << "\n\nSequential DFS output : ";
-    DFS(graph, startNode);
-    end = clock();
 
-    total1 = double(end - start) / double(CLOCKS_PER_SEC);
-    cout << "\nSequential time : " << fixed << setprecision(10) << total1;
 
-    start = clock();
-    cout << "\nParallel DFS output : ";
-    parallelBFS(graph, startNode);
-    end = clock();
+    start=clock();
+    cout<<"\n\nsequential DFS output : ";
+    DFS(graph,0);
+    end=clock();
 
-    total2 = double(end - start) / double(CLOCKS_PER_SEC);
-    cout << "\nParallel time : " << fixed << setprecision(10) << total2;
+    total1=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nSequential time : "<<fixed<<setprecision(10)<<total1;
 
-    cout << "\nSpeedup : " << total1 / total2;
+    start=clock();
+    cout<<"\nparallel DFS output : ";
+    parallelBFS(graph,0);
+    end=clock();
+
+    total2=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nParallel time : "<<fixed<<setprecision(10)<<total2;
+
+    cout<<"\nSpeedup : "<<total1/total2;
 
     return 0;
 }
